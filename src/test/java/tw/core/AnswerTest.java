0f1ae4c6068f0ae6533ecb953349b76ecd81ca6b
Terry Teng <terry.teng@oocl.com>
2018-07-12 15:40:37 +0800

    [Terry] AnswerTest 1: should_not_throw_the_exception_when_call_validate()

--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -2,6 +2,10 @@ package tw.core;
 
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import tw.core.exception.AnswerFormatIncorrectException;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 /**
  * Created by jxzhong on 2017/9/23.
@@ -12,6 +16,15 @@ public class AnswerTest {
     @BeforeEach
     public void setUp() {
         actualAnswer = Answer.createAnswer("1 2 3 4");
+    }
+
+    @Test
+    public void should_not_throw_the_exception_when_call_validate(){
+        try {
+            actualAnswer.validate();
+        } catch (AnswerFormatIncorrectException answerFormatIncorrectException){
+            fail("should not throw the exception");
+        }
     }
 
 }
